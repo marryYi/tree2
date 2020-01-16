bfbeb4863125ce5bcb3a03b9ea7e254130e6d655
marryYi <2086265675@qq.com>
2020-01-16 18:45:07 +0800

    添加项目文件。

---
+++ tree2Dlg.cpp
@@ -0,0 +1,181 @@
+﻿
+// tree2Dlg.cpp: 实现文件
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "tree2.h"
+#include "tree2Dlg.h"
+#include "afxdialogex.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// 用于应用程序“关于”菜单项的 CAboutDlg 对话框
+
+class CAboutDlg : public CDialogEx
+{
+public:
+	CAboutDlg();
+
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_ABOUTBOX };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 支持
+
+// 实现
+protected:
+	DECLARE_MESSAGE_MAP()
+};
+
+CAboutDlg::CAboutDlg() : CDialogEx(IDD_ABOUTBOX)
+{
+}
+
+void CAboutDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+BEGIN_MESSAGE_MAP(CAboutDlg, CDialogEx)
+END_MESSAGE_MAP()
+
+
+// Ctree2Dlg 对话框
+
+
+
+Ctree2Dlg::Ctree2Dlg(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_TREE2_DIALOG, pParent)
+{
+	m_hIcon = AfxGetApp()->LoadIcon(IDR_MAINFRAME);
+}
+
+void Ctree2Dlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+	DDX_Control(pDX, IDC_TREE1, m_tree);
+}
+
+BEGIN_MESSAGE_MAP(Ctree2Dlg, CDialogEx)
+	ON_WM_SYSCOMMAND()
+	ON_WM_PAINT()
+	ON_WM_QUERYDRAGICON()
+END_MESSAGE_MAP()
+
+
+// Ctree2Dlg 消息处理程序
+
+BOOL Ctree2Dlg::OnInitDialog()
+{
+	CDialogEx::OnInitDialog();
+
+	// 将“关于...”菜单项添加到系统菜单中。
+
+	// IDM_ABOUTBOX 必须在系统命令范围内。
+	ASSERT((IDM_ABOUTBOX & 0xFFF0) == IDM_ABOUTBOX);
+	ASSERT(IDM_ABOUTBOX < 0xF000);
+
+	CMenu* pSysMenu = GetSystemMenu(FALSE);
+	if (pSysMenu != nullptr)
+	{
+		BOOL bNameValid;
+		CString strAboutMenu;
+		bNameValid = strAboutMenu.LoadString(IDS_ABOUTBOX);
+		ASSERT(bNameValid);
+		if (!strAboutMenu.IsEmpty())
+		{
+			pSysMenu->AppendMenu(MF_SEPARATOR);
+			pSysMenu->AppendMenu(MF_STRING, IDM_ABOUTBOX, strAboutMenu);
+		}
+	}
+
+	// 设置此对话框的图标。  当应用程序主窗口不是对话框时，框架将自动
+	//  执行此操作
+	SetIcon(m_hIcon, TRUE);			// 设置大图标
+	SetIcon(m_hIcon, FALSE);		// 设置小图标
+
+	// TODO: 在此添加额外的初始化代码
+	HTREEITEM root;
+	HTREEITEM first_sub;
+	HTREEITEM second_sub;
+	m_tree.ModifyStyle(0, TVS_HASLINES | TVS_HASBUTTONS | TVS_LINESATROOT);
+
+	m_imagelist.Create(32, 32, ILC_COLOR32 | ILC_MASK, 4, 4);
+	for(int i=0;i<4;i++)
+	{
+		HICON icon = theApp.LoadIcon(IDC_ICON1 + 1);
+		m_imagelist.Add(icon);
+
+	}
+
+	root = m_tree.InsertItem(_T("Root"));
+	first_sub = m_tree.InsertItem(_T("First_Sub"), root);
+	m_tree.InsertItem(_T("1"), root);
+	m_tree.InsertItem(_T("2"), root);
+	m_tree.InsertItem(_T("3"), root);
+	m_tree.InsertItem(_T("4"), root);
+	second_sub = m_tree.InsertItem(_T("Seond"), first_sub);
+	m_tree.InsertItem(_T("5"), first_sub);
+	m_tree.InsertItem(_T("6"), first_sub);
+	m_tree.InsertItem(_T("7"), first_sub);
+	m_tree.InsertItem(_T("8"), first_sub);
+	//m_tree.InsertItem(_T("tree"));
+
+	return TRUE;  // 除非将焦点设置到控件，否则返回 TRUE
+}
+
+void Ctree2Dlg::OnSysCommand(UINT nID, LPARAM lParam)
+{
+	if ((nID & 0xFFF0) == IDM_ABOUTBOX)
+	{
+		CAboutDlg dlgAbout;
+		dlgAbout.DoModal();
+	}
+	else
+	{
+		CDialogEx::OnSysCommand(nID, lParam);
+	}
+}
+
+// 如果向对话框添加最小化按钮，则需要下面的代码
+//  来绘制该图标。  对于使用文档/视图模型的 MFC 应用程序，
+//  这将由框架自动完成。
+
+void Ctree2Dlg::OnPaint()
+{
+	if (IsIconic())
+	{
+		CPaintDC dc(this); // 用于绘制的设备上下文
+
+		SendMessage(WM_ICONERASEBKGND, reinterpret_cast<WPARAM>(dc.GetSafeHdc()), 0);
+
+		// 使图标在工作区矩形中居中
+		int cxIcon = GetSystemMetrics(SM_CXICON);
+		int cyIcon = GetSystemMetrics(SM_CYICON);
+		CRect rect;
+		GetClientRect(&rect);
+		int x = (rect.Width() - cxIcon + 1) / 2;
+		int y = (rect.Height() - cyIcon + 1) / 2;
+
+		// 绘制图标
+		dc.DrawIcon(x, y, m_hIcon);
+	}
+	else
+	{
+		CDialogEx::OnPaint();
+	}
+}
+
+//当用户拖动最小化窗口时系统调用此函数取得光标
+//显示。
+HCURSOR Ctree2Dlg::OnQueryDragIcon()
+{
+	return static_cast<HCURSOR>(m_hIcon);
+}
+
